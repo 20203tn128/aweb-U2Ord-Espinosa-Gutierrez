29dbb0e34ce767dde55a7b9ef1e1a5749cf8f71c
Liz Claudia <20203tn128@utez.edu.mx>
2021-07-05 11:21:33 -0500

    Commit para agregar  el examen

---
+++ JS/miapp.js
@@ -0,0 +1,14 @@
+function guardar(){
+    let nota = document.getElementById('nota').value;
+    
+    const note = {
+        nota,
+        time = Date(),
+    };
+
+}
+ function mostrarNota(){
+      <div class="form-group">
+          <textarea placeholder="<nota>"></textarea>
+      </div>
+ }
